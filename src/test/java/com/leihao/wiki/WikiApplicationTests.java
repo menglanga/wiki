d4216095a8a6601fdceed353a6bec8e31a0bea50
“leihao“ <245654808@qq.com>
2022-12-13 21:54:50 +0800

    初始化提交（啥也没干）

---
+++ src/test/java/com/leihao/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leihao.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
