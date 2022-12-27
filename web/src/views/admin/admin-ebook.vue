386fa48be7b3c56d83dee6346d424f0637458be9
“leihao“ <245654808@qq.com>
2022-12-27 20:20:50 +0800

    模态框显示电子书表单，编辑某一行时显示该行数据

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -16,7 +16,7 @@
         </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
-            <a-button type="primary" @click="edit">编辑</a-button>
+            <a-button type="primary" @click="edit(record)">编辑</a-button>
             <a-button type="danger">删除</a-button>
           </a-space>
         </template>
@@ -29,7 +29,23 @@
           :confirm-loading="modelLoading"
           @ok="handleModelOk"
   >
-    <p>test</p>
+    <a-form :model="ebook" :label-col="{span: 6}">
+      <a-form-item label="封面">
+        <a-input v-model:value="ebook.cover"/>
+      </a-form-item>
+      <a-form-item label="名称">
+        <a-input v-model:value="ebook.name"/>
+      </a-form-item>
+      <a-form-item label="分类一">
+        <a-input v-model:value="ebook.category1Id"/>
+      </a-form-item>
+      <a-form-item label="分类二">
+        <a-input v-model:value="ebook.category2Id"/>
+      </a-form-item>
+      <a-form-item label="描述">
+        <a-input v-model:value="ebook.description" type="text"/>
+      </a-form-item>
+    </a-form>
   </a-modal>
 </template>
 
@@ -115,6 +131,7 @@
       };
 
       //表单
+      const ebook=ref({});
       const modelVisible = ref(false);
       const modelLoading = ref(false);
 
@@ -128,8 +145,9 @@
 
 
       //编辑
-      const edit=()=>{
+      const edit=(record: any)=>{
         modelVisible.value=true;
+        ebook.value=record;
       };
 
       onMounted(()=>{
@@ -148,7 +166,9 @@
           edit,
           modelVisible,
           modelLoading,
-          handleModelOk}
+          handleModelOk,
+          ebook
+        }
       }
     })
 </script>
