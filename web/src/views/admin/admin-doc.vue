f7d3a954d12830e57435b1e3907336eb3e5c9a71
“leihao“ <245654808@qq.com>
2023-01-05 21:04:35 +0800

    增加页面参数传递完成新增文档功能，新增方法中修复bug(导入所有文档)

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -1,79 +1,79 @@
 <template>
-  <a-layout>
-    <a-layout-content
-            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    <a-layout>
+        <a-layout-content
+                :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+            <p>
+                <a-form
+                        layout="inline"
+                        :model="param"
+                >
+                    <a-form-item>
+                        <a-button
+                                type="primary"
+                                @click="handleQuery()"
+                        >
+                            查询
+                        </a-button>
+                    </a-form-item>
+                    <a-form-item>
+                        <a-button type="primary" @click="add()">新增</a-button>
+                    </a-form-item>
+                </a-form>
+            </p>
+            <a-table
+                    :columns="columns"
+                    :row-key="record => record.id"
+                    :data-source="level1"
+                    :pagination="false"
+                    :loading="loading"
+            >
+                <template #cover="{ text: cover }">
+                    <img v-if="cover" :src="cover" alt="avatar"/>
+                </template>
+                <template v-slot:action="{ text, record }">
+                    <a-space size="small">
+                        <a-button type="primary" @click="edit(record)">编辑</a-button>
+                        <a-popconfirm
+                                title="删除后不可恢复，确认删除？"
+                                ok-text="是"
+                                cancel-text="否"
+                                @confirm="handleDelete(record.id)"
+                        >
+                            <a-button type="danger">删除</a-button>
+                        </a-popconfirm>
+                    </a-space>
+                </template>
+            </a-table>
+        </a-layout-content>
+    </a-layout>
+    <a-modal
+            title="文档表单"
+            v-model:visible="modelVisible"
+            :confirm-loading="modelLoading"
+            @ok="handleModelOk"
     >
-      <p>
-        <a-form
-                layout="inline"
-                :model="param"
-        >
-          <a-form-item>
-            <a-button
-                    type="primary"
-                    @click="handleQuery()"
-            >
-              查询
-            </a-button>
-          </a-form-item>
-          <a-form-item>
-            <a-button type="primary" @click="add()" >新增</a-button>
-          </a-form-item>
+        <a-form :model="doc" :label-col="{span: 6}">
+            <a-form-item label="名称">
+                <a-input v-model:value="doc.name"/>
+            </a-form-item>
+            <a-form-item label="父文档">
+                <a-tree-select
+                        v-model:value="doc.parent"
+                        style="width: 100%"
+                        :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+                        :tree-data="treeSelectData"
+                        placeholder="请选择父文档"
+                        tree-default-expand-all
+                        :replaceFields="{title:'name',key:'id',value: 'id',}"
+                >
+                </a-tree-select>
+            </a-form-item>
+            <a-form-item label="顺序">
+                <a-input v-model:value="doc.sort"/>
+            </a-form-item>
         </a-form>
-      </p>
-      <a-table
-              :columns="columns"
-              :row-key="record => record.id"
-              :data-source="level1"
-              :pagination="false"
-              :loading="loading"
-      >
-        <template #cover="{ text: cover }">
-          <img v-if="cover" :src="cover" alt="avatar" />
-        </template>
-        <template v-slot:action="{ text, record }">
-          <a-space size="small">
-            <a-button type="primary" @click="edit(record)">编辑</a-button>
-            <a-popconfirm
-                    title="删除后不可恢复，确认删除？"
-                    ok-text="是"
-                    cancel-text="否"
-                    @confirm="handleDelete(record.id)"
-            >
-              <a-button type="danger">删除</a-button>
-            </a-popconfirm>
-          </a-space>
-        </template>
-      </a-table>
-    </a-layout-content>
-  </a-layout>
-  <a-modal
-          title="文档表单"
-          v-model:visible="modelVisible"
-          :confirm-loading="modelLoading"
-          @ok="handleModelOk"
-  >
-    <a-form :model="doc" :label-col="{span: 6}">
-      <a-form-item label="名称">
-        <a-input v-model:value="doc.name"/>
-      </a-form-item>
-      <a-form-item label="父文档">
-        <a-tree-select
-                v-model:value="doc.parent"
-                style="width: 100%"
-                :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
-                :tree-data="treeSelectData"
-                placeholder="请选择父文档"
-                tree-default-expand-all
-                :replaceFields="{title:'name',key:'id',value: 'id',}"
-        >
-        </a-tree-select>
-      </a-form-item>
-      <a-form-item label="顺序">
-        <a-input v-model:value="doc.sort"/>
-      </a-form-item>
-    </a-form>
-  </a-modal>
+    </a-modal>
 </template>
 
 <script lang="ts">
@@ -81,170 +81,172 @@
     import axios from 'axios';
     import {message} from 'ant-design-vue';
     import {Tool} from "@/util/tool";
+    import {useRoute} from "vue-router";
 
-    export  default defineComponent({
-    name: 'AdminDoc',
-    setup(){
-      const param=ref();
-      param.value={};
-      const docs=ref();
-      const loading=ref(false);
+    export default defineComponent({
+        name: 'AdminDoc',
+        setup() {
+            const route = useRoute();
+            const param = ref();
+            param.value = {};
+            const docs = ref();
+            const loading = ref(false);
 
-      const columns=[
-        {
-          title: '名称',
-          dataIndex: 'name'
-        },
-        {
-          title: '父文档',
-          key: 'parent',
-          dataIndex: 'parent'
-        },
-        {
-          title: '顺序',
-          dataIndex: 'sort',
-        },
-        {
-          title: 'Action',
-          key: 'action',
-          slots: { customRender: 'action' }
-        }
-      ];
+            const columns = [
+                {
+                    title: '名称',
+                    dataIndex: 'name'
+                },
+                {
+                    title: '父文档',
+                    key: 'parent',
+                    dataIndex: 'parent'
+                },
+                {
+                    title: '顺序',
+                    dataIndex: 'sort',
+                },
+                {
+                    title: 'Action',
+                    key: 'action',
+                    slots: {customRender: 'action'}
+                }
+            ];
 
-      /**
-       * 一级文档树，children属性就是二级文档
-       * [{
-       *     id: "",
-       *     name: "",
-       *     children:[{
-       *         id: "",
-       *         name: ""
-       *     }]
-       * }]
-       */
-      const level1=ref();
+            /**
+             * 一级文档树，children属性就是二级文档
+             * [{
+             *     id: "",
+             *     name: "",
+             *     children:[{
+             *         id: "",
+             *         name: ""
+             *     }]
+             * }]
+             */
+            const level1 = ref();
 
-    //  数据查询
-      const handleQuery=()=>{
-        loading.value=true;
-        axios.get("/doc/all").then((response)=>{
-          loading.value=false;
-          const data=response.data;
-          if (data.success){
-            docs.value=data.data;
-            console.log("原始数组：",docs.value);
-            level1.value=[];
-            level1.value=Tool.array2tree(docs.value,0);
-            console.log("树形结构：",level1.value);
-          }else{
-            message.error(data.message);
-          }
-
-
-
-        });
-      };
+            //  数据查询
+            const handleQuery = () => {
+                loading.value = true;
+                axios.get("/doc/all").then((response) => {
+                    loading.value = false;
+                    const data = response.data;
+                    if (data.success) {
+                        docs.value = data.data;
+                        console.log("原始数组：", docs.value);
+                        level1.value = [];
+                        level1.value = Tool.array2tree(docs.value, 0);
+                        console.log("树形结构：", level1.value);
+                    } else {
+                        message.error(data.message);
+                    }
 
 
-      //表单
-      const treeSelectData=ref();
-      treeSelectData.value=[];
-      const doc=ref({});
-      const modelVisible = ref(false);
-      const modelLoading = ref(false);
-
-      const handleModelOk = () => {
-        modelLoading.value = true;
-        axios.post("/doc/save",doc.value).then((response)=>{
-          modelLoading.value=false;
-          const data=response.data;
-          if (data.success){
-            modelVisible.value=false;
-
-            //重新加载列表
-            handleQuery();
-          }else{
-            message.error(data.message);
-          }
-        });
-      };
+                });
+            };
 
 
-      const  setDisable=(treeSelectData :any,id:any)=>{
-        for (let i=0;i<treeSelectData.length;i++){
-          const node=treeSelectData[i];
-          if (node.id===id){
-            console.log("disabled",node);
-            node.disabled=true;
-            const children=node.children;
-            if (Tool.isNotEmpty(children)){
-              for (let j = 0; j < children.length; j++){
-                setDisable(children,children[j].id);
-              }
+            //表单
+            const treeSelectData = ref();
+            treeSelectData.value = [];
+            const doc = ref({});
+            const modelVisible = ref(false);
+            const modelLoading = ref(false);
+
+            const handleModelOk = () => {
+                modelLoading.value = true;
+                axios.post("/doc/save", doc.value).then((response) => {
+                    modelLoading.value = false;
+                    const data = response.data;
+                    if (data.success) {
+                        modelVisible.value = false;
+
+                        //重新加载列表
+                        handleQuery();
+                    } else {
+                        message.error(data.message);
+                    }
+                });
+            };
+
+
+            const setDisable = (treeSelectData: any, id: any) => {
+                for (let i = 0; i < treeSelectData.length; i++) {
+                    const node = treeSelectData[i];
+                    if (node.id === id) {
+                        console.log("disabled", node);
+                        node.disabled = true;
+                        const children = node.children;
+                        if (Tool.isNotEmpty(children)) {
+                            for (let j = 0; j < children.length; j++) {
+                                setDisable(children, children[j].id);
+                            }
+                        }
+                    } else {
+                        const children = node.children;
+                        if (Tool.isNotEmpty(children)) {
+                            setDisable(children,id);
+                        }
+                    }
+                }
+            };
+
+
+            //编辑
+            const edit = (record: any) => {
+                modelVisible.value = true;
+                doc.value = Tool.copy(record);
+                //不能选择当前节点节气所有子孙节点作为父节点
+                treeSelectData.value = Tool.copy(level1.value);
+                setDisable(treeSelectData.value, record.id);
+
+                //为选择树添加一个无
+                treeSelectData.value.unshift({id: 0, name: '无'});
+            };
+
+            //新增
+            const add = () => {
+                modelVisible.value = true;
+                treeSelectData.value = Tool.copy(level1.value);
+                treeSelectData.value.unshift({id: 0, name: '无'});
+
+                doc.value = {
+                    ebookId: route.query.ebookId
+                };
+            };
+
+
+            //删除
+            const handleDelete = (id: any) => {
+                axios.delete("/doc/delete/" + id).then((response) => {
+                    const data = response.data;
+                    if (data.success) {
+                        //重新加载列表
+                        handleQuery();
+                    }
+                });
+            };
+
+            onMounted(() => {
+                handleQuery();
+            });
+
+            return {
+                level1,
+                param,
+                columns,
+                loading,
+                edit,
+                add,
+                handleDelete,
+                modelVisible,
+                modelLoading,
+                handleModelOk,
+                handleQuery,
+                doc,
+                treeSelectData
             }
-            else {
-              const children=node.children;
-              if (Tool.isNotEmpty(children)){
-                setDisable(children,children.id);
-              }
-
-            }
-          }
         }
-      };
-
-
-
-
-      //编辑
-      const edit=(record: any)=>{
-        modelVisible.value=true;
-        doc.value=Tool.copy(record);
-        //不能选择当前节点节气所有子孙节点作为父节点
-        treeSelectData.value=Tool.copy(level1.value);
-        setDisable(treeSelectData.value,record.id);
-
-        //为选择树添加一个无
-        treeSelectData.value.unshift({id:0,name:'无'});
-      };
-
-      //新增
-      const add=()=>{
-        modelVisible.value=true;
-        doc.value={};
-      };
-
-
-      //新增
-      const handleDelete=(id: any)=>{
-        axios.delete("/doc/delete/"+id).then((response)=>{
-          const data=response.data;
-          if (data.success){
-            //重新加载列表
-            handleQuery();
-          }
-        });
-      };
-
-      onMounted(()=>{
-        handleQuery();
-      });
-
-      return{
-          level1,
-          param,
-          columns,
-          loading,
-          edit,
-          add,
-          handleDelete,
-          modelVisible,
-          modelLoading,
-          handleModelOk,
-          handleQuery,
-          doc,
-          treeSelectData
-        }
-      }
     })
 </script>
