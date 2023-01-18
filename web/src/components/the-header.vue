9b63f707a551594fb74e5c782913f866b1eb2975
“leihao“ <245654808@qq.com>
2023-01-18 20:43:20 +0800

    前后端联调登录功能

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -52,13 +52,18 @@
 
 <script lang="ts">
     import { defineComponent,ref } from 'vue';
+    import axios from 'axios';
+    import {message} from 'ant-design-vue';
+
+    declare let hexMd5: any;
+    declare let KEY: any;
 
     export default defineComponent({
         name: 'the-header',
         setup(){
             const loginUser=ref({
-                loginName: "test",
-                password: "test",
+                loginName: "",
+                password: "",
             });
             const loginModelVisible=ref(false);
             const loginModelLoading=ref(false);
@@ -68,6 +73,21 @@
 
             const login=()=>{
                 console.log("开始登录");
+
+                loginModelLoading.value = true;
+
+                loginUser.value.password=hexMd5(loginUser.value.password+KEY);
+
+                axios.post("/user/login", loginUser.value).then((response) => {
+                    loginModelLoading.value = false;
+                    const data = response.data;
+                    if (data.success) {
+                        loginModelVisible.value = false;
+                        message.success("登录成功！")
+                    } else {
+                        message.error(data.message);
+                    }
+                });
             };
 
             return{
