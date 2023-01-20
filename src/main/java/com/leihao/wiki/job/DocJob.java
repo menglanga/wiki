8cd446cf4e398a41fe46a677fa34550cb13768ce
“leihao“ <245654808@qq.com>
2023-01-20 22:43:09 +0800

    首页显示电子书信息：文档数 阅读数 点赞数 点赞接口不需要登录拦截

--- src/main/java/com/leihao/wiki/job/DocJob.java
+++ src/main/java/com/leihao/wiki/job/DocJob.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
 
 @Component
 public class DocJob {
