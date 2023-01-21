001e09a29df628f85676d72208b0dddd114b597e
“leihao“ <245654808@qq.com>
2023-01-21 17:49:16 +0800

    事务提交

--- src/main/java/com/leihao/wiki/service/DocService.java
+++ src/main/java/com/leihao/wiki/service/DocService.java
@@ -26,6 +26,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -85,6 +86,7 @@ public class DocService {
         return docResponseList;
     }
 
+    @Transactional
     public void save(DocSaveRequest request) {
         Doc doc = CopyUtil.copy(request, Doc.class);
         Content content = CopyUtil.copy(request, Content.class);
