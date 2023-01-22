7e8f353b194add837defabaea32c13fcbcbae8d2
“leihao“ <245654808@qq.com>
2023-01-22 17:12:29 +0800

    将欢迎文字提取为组件

---
+++ web/src/components/the-welcome.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>欢迎红红火火恍恍惚惚或或或或</h1>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+
+
+    export default defineComponent({
+        name: 'the-welcome',
+        setup(){
+
+            return{
+
+            }
+        }
+    });
+</script>
--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -1,185 +1,192 @@
 <template>
-  <a-layout>
-    <a-layout-sider width="200" style="background: #fff">
-      <a-menu
-              mode="inline"
-              :style="{ height: '100%', borderRight: 0 }"
-              @click="handleClick"
-      >
-        <a-menu-item key="welcome">
-          <MailOutlined/><span>欢迎</span>
-        </a-menu-item>
-        <a-sub-menu v-for="item in level1" :key="item.id">
-          <template v-slot:title>
+    <a-layout>
+        <a-layout-sider width="200" style="background: #fff">
+            <a-menu
+                    mode="inline"
+                    :style="{ height: '100%', borderRight: 0 }"
+                    @click="handleClick"
+            >
+                <a-menu-item key="welcome">
+                    <MailOutlined/>
+                    <span>欢迎</span>
+                </a-menu-item>
+                <a-sub-menu v-for="item in level1" :key="item.id">
+                    <template v-slot:title>
               <span>
-                <user-outlined />
+                <user-outlined/>
                 {{item.name}}
               </span>
-          </template>
-          <a-menu-item v-for="child in item.children" :key="child.id">
-            <MailOutlined/><span>{{child.name}}</span>
-          </a-menu-item>
-        </a-sub-menu>
-      </a-menu>
-    </a-layout-sider>
-    <a-layout-content
-            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-    >
-      <div class="welcome" v-show="isShowWelcome">
-        <h1>欢迎使用知识库</h1>
-      </div>
-      <a-list v-show="!isShowWelcome"
-              item-layout="vertical"
-              size="large"
-              :grid="{gutter:30,column:3}"
-              :data-source="ebooks">
-        <template #renderItem="{ item }">
-          <a-list-item key="item.name">
-            <template #actions>
+                    </template>
+                    <a-menu-item v-for="child in item.children" :key="child.id">
+                        <MailOutlined/>
+                        <span>{{child.name}}</span>
+                    </a-menu-item>
+                </a-sub-menu>
+            </a-menu>
+        </a-layout-sider>
+        <a-layout-content
+                :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+            <div class="welcome" v-show="isShowWelcome">
+                <the-welcome></the-welcome>
+            </div>
+            <a-list v-show="!isShowWelcome"
+                    item-layout="vertical"
+                    size="large"
+                    :grid="{gutter:30,column:3}"
+                    :data-source="ebooks">
+                <template #renderItem="{ item }">
+                    <a-list-item key="item.name">
+                        <template #actions>
                 <span>
-                    <component v-bind:is="'FileOutlined'" style="margin-right: 8px" />
+                    <component v-bind:is="'FileOutlined'" style="margin-right: 8px"/>
                     {{ item.docCount }}
                 </span>
-                <span>
-                    <component v-bind:is="'LikeOutlined'" style="margin-right: 8px" />
+                            <span>
+                    <component v-bind:is="'LikeOutlined'" style="margin-right: 8px"/>
                     {{ item.voteCount }}
                 </span>
-                <span>
-                    <component v-bind:is="'UserOutlined'" style="margin-right: 8px" />
+                            <span>
+                    <component v-bind:is="'UserOutlined'" style="margin-right: 8px"/>
                     {{ item.viewCount }}
                 </span>
-            </template>
-            <a-list-item-meta :description="item.description">
-              <template #title>
-                <router-link :to="'/doc?ebookId='+item.id">
-                  {{item.name}}
-                </router-link>
-              </template>
-              <template #avatar><a-avatar :src="item.cover" /></template>
-            </a-list-item-meta>
-          </a-list-item>
-        </template>
-      </a-list>
-    </a-layout-content>
-  </a-layout>
+                        </template>
+                        <a-list-item-meta :description="item.description">
+                            <template #title>
+                                <router-link :to="'/doc?ebookId='+item.id">
+                                    {{item.name}}
+                                </router-link>
+                            </template>
+                            <template #avatar>
+                                <a-avatar :src="item.cover"/>
+                            </template>
+                        </a-list-item-meta>
+                    </a-list-item>
+                </template>
+            </a-list>
+        </a-layout-content>
+    </a-layout>
 
 </template>
 
 <script lang="ts">
-import { defineComponent,onMounted,ref ,reactive,toRef} from 'vue';
-import axios from 'axios';
-import {message} from 'ant-design-vue';
-import {Tool} from "@/util/tool";
+    import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
+    import axios from 'axios';
+    import {message} from 'ant-design-vue';
+    import {Tool} from "@/util/tool";
+    import TheWelcome from '@/components/the-welcome.vue';
 
-const listData: any = [];
+    const listData: any = [];
 
-// for (let i = 0; i < 23; i++) {
-//   listData.push({
-//     href: 'https://www.antdv.com/',
-//     title: `ant design vue part ${i}`,
-//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-//     description:
-//             'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-//     content:
-//             'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-//   });
-// }
+    // for (let i = 0; i < 23; i++) {
+    //   listData.push({
+    //     href: 'https://www.antdv.com/',
+    //     title: `ant design vue part ${i}`,
+    //     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    //     description:
+    //             'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    //     content:
+    //             'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+    //   });
+    // }
 
-export default defineComponent({
-  name: 'Home',
-  setup(){
+    export default defineComponent({
+        name: 'Home',
+        components: {
+            TheWelcome,
 
-    const ebooks=ref();
-    const ebooks1=reactive({books:[]})
-
-
-    const level1=ref();
-
-    let categorys :any;
-
-    //  数据查询所有分类
-    const handleQueryCategory=()=>{
-      axios.get("/category/all").then((response)=>{
-        const data=response.data;
-        if (data.success){
-          categorys=data.data;
-          console.log("原始数组：",categorys);
-          level1.value=[];
-          level1.value=Tool.array2tree(categorys,0);
-          console.log("树形结构：",level1.value);
-        }else{
-          message.error(data.message);
-        }
-      });
-    };
-
-    const isShowWelcome=ref(true);
-    let category2Id=0;
-
-    const handleQueryEbook=()=>{
-      axios.get("/ebook/list",{
-        params:{
-          pageNum: 1,
-          pageSize: 999,
-          category2Id: category2Id
-        }
-      }).then((response)=>{
-        const data=response.data;
-        ebooks.value=data.data.list
-        //ebooks1.books=data.data
-      })
-    };
-
-    const handleClick=(value: any)=>{
-      console.log("menu click",value);
-      if (value.key==='welcome') {
-        isShowWelcome.value=true;
-      }else {
-        category2Id=value.key;
-        isShowWelcome.value=false;
-        handleQueryEbook();
-      }
-    };
-
-
-
-
-    onMounted(()=>{
-      handleQueryCategory();
-    });
-
-    return {
-      ebooks,
-      // ebooks2:toRef(ebooks1,"books"),
-      // listData,
-      pagination : {
-        onChange: (page: any) => {
-          console.log(page);
         },
-        pageSize: 3,
-      },
-    //   actions: [
-    //   { type: 'StarOutlined', text: '156' },
-    //   { type: 'LikeOutlined', text: '156' },
-    //   { type: 'MessageOutlined', text: '2' },
-    // ],
-      handleClick,
-      level1,
-      isShowWelcome
-    }
-  }
+        setup() {
+
+            const ebooks = ref();
+            const ebooks1 = reactive({books: []})
 
 
-});
+            const level1 = ref();
+
+            let categorys: any;
+
+            //  数据查询所有分类
+            const handleQueryCategory = () => {
+                axios.get("/category/all").then((response) => {
+                    const data = response.data;
+                    if (data.success) {
+                        categorys = data.data;
+                        console.log("原始数组：", categorys);
+                        level1.value = [];
+                        level1.value = Tool.array2tree(categorys, 0);
+                        console.log("树形结构：", level1.value);
+                    } else {
+                        message.error(data.message);
+                    }
+                });
+            };
+
+            const isShowWelcome = ref(true);
+            let category2Id = 0;
+
+            const handleQueryEbook = () => {
+                axios.get("/ebook/list", {
+                    params: {
+                        pageNum: 1,
+                        pageSize: 999,
+                        category2Id: category2Id
+                    }
+                }).then((response) => {
+                    const data = response.data;
+                    ebooks.value = data.data.list
+                    //ebooks1.books=data.data
+                })
+            };
+
+            const handleClick = (value: any) => {
+                console.log("menu click", value);
+                if (value.key === 'welcome') {
+                    isShowWelcome.value = true;
+                } else {
+                    category2Id = value.key;
+                    isShowWelcome.value = false;
+                    handleQueryEbook();
+                }
+            };
+
+
+            onMounted(() => {
+                handleQueryCategory();
+            });
+
+            return {
+                ebooks,
+                // ebooks2:toRef(ebooks1,"books"),
+                // listData,
+                pagination: {
+                    onChange: (page: any) => {
+                        console.log(page);
+                    },
+                    pageSize: 3,
+                },
+                //   actions: [
+                //   { type: 'StarOutlined', text: '156' },
+                //   { type: 'LikeOutlined', text: '156' },
+                //   { type: 'MessageOutlined', text: '2' },
+                // ],
+                handleClick,
+                level1,
+                isShowWelcome
+            }
+        }
+
+
+    });
 </script>
 
 <style scoped>
-  .ant-avatar {
-    width: 50px;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 8%;
-    margin:  5px 0;
-  }
+    .ant-avatar {
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 8%;
+        margin: 5px 0;
+    }
 </style>
 
